90858b16b73e3c4224d12f085ea3975f74d293e2
Tony Maluki <tonymaluki@gmail.com>
2023-08-20 20:18:18 +0300

    button variants

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,10 +1,69 @@
-import { Button,Stack } from "@mui/material";
+import { Button, Stack, IconButton, ButtonGroup } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
 function MuiButton() {
   return (
-    <Stack spacing={2} direction='row'>
-      <Button variant="text" href="https://google.com">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="outlined" color="primary">
+          primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="warning">
+          warning
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+      </Stack>
+
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          small
+        </Button>
+        <Button variant="contained" size="medium">
+          medium
+        </Button>
+        <Button variant="contained" size="large">
+          large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" startIcon={<SendIcon />}>
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
+
+        <IconButton aria-label="send" color="warning" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+
+      <Stack direction="row">
+        <ButtonGroup variant="outlined" orientation="horizontal" size='small' color="secondary">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
     </Stack>
   );
 }
