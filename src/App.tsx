25abc520ce6e8cd331797a2b1062f5e2981424fa
Tony Maluki <tonymaluki@gmail.com>
2023-08-20 13:57:37 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MuiButton from "./components/MuiButton";
+import MuiTypography from "./components/MuiTypography";
+
+function App() {
+  return (
+    <>
+      {/* <MuiTypography /> */}
+      <MuiButton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,27 @@
+import { Typography } from "@mui/material";
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 heading</Typography>
+      <Typography variant="h2">h2 heading</Typography>
+      <Typography variant="h3">h3 heading</Typography>
+      <Typography variant="h4">h4 heading</Typography>
+      <Typography variant="h5">h5 heading</Typography>
+      <Typography variant="h6">h6 heading</Typography>
+
+      <Typography variant="subtitle1">sub title 1</Typography>
+      <Typography variant="subtitle2">sub title 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
+        perferendis.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores,
+        perferendis.
+      </Typography>
+    </div>
+  );
+};
+
+export default MuiTypography;
