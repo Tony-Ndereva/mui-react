93f04ca9048ea7aaf0fc371a5d9f08de174a217c
Tony <tonymaluki@gmail.com>
2023-09-14 21:18:19 +0300

    textfieds variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 import MuiButton from "./components/MuiButton";
+import MuiTextField from "./components/MuiTextField";
 import MuiTypography from "./components/MuiTypography";
 
 function App() {
   return (
     <>
-      {/* <MuiTypography /> */}
+      <MuiTypography />
       <MuiButton />
+      <MuiTextField />
     </>
   );
 }
--- src/components/MuiTextField.tsx
+++ src/components/MuiTextField.tsx
@@ -1,6 +1,17 @@
-import {Stack} from "@mui/material";
+import { Stack, TextField } from "@mui/material";
 const MuiTextField = () => {
-    return ( <Stack spacing={4}>MuiTextField</Stack> );
-}
- 
+  return (
+    <Stack spacing={4}>
+      <Stack direction="row" spacing={2}>
+        <TextField label='Name' variant="outlined"/>
+        <TextField label='Name' variant="filled"/>
+        <TextField label='Name' variant="standard"/>
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField label='Small Secondary' variant="outlined"/>
+      </Stack>
+    </Stack>
+  );
+};
+
 export default MuiTextField;
